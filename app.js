5e0e9f3bd7b4e44e6bf5dd0be496a6f48e87786c
KokoTheBest <81335306+KokoTheBest@users.noreply.github.com>
2022-08-09 16:01:24 -0400

    Single server.

--- app.js
+++ app.js
@@ -12,17 +12,18 @@ dotenv.config();
 const app = express()
 
 app.use(cors({credentials: true, origin: 'http://localhost:3000'}, "*"))
-app.use("/api/storage", express.static("storage"))
+app.use("/storage", express.static("storage"))
 app.use(express.json({limit: '0.151mb'}));
 app.use(express.urlencoded({limit: '0.151mb', extended: true}));
 app.use(cookieParser())
-app.use("/api/posts", postsRouter)
-app.use("/api", authrouter)
+app.use("/posts", postsRouter)
+app.use("/", authrouter)
 mongoose.connect(process.env.MONGODB_URI)
 
+app.use(express.static('frontend/build'));
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(path.resolve(), 'frontend', 'build', 'index.html'));
-});
+  res.sendFile(path.resolve('frontend', 'build', 'index.html'));
+})
 
 app.listen(process.env.PORT || 8000, "0.0.0.0", () => {
     console.log("Server is running.")
