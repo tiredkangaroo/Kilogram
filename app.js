52a106f78253091a2659fd40d24edbed3b1b29b4
KokoTheBest <81335306+KokoTheBest@users.noreply.github.com>
2022-08-09 13:38:24 -0400

    Changed all backend routes to /api.

--- app.js
+++ app.js
@@ -10,26 +10,14 @@ dotenv.config();
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, "./frontend/build")))
 app.use(cors({credentials: true, origin: 'http://localhost:3000'}, "*"))
-app.use("/storage", express.static("storage"))
+app.use("/api/storage", express.static("storage"))
 app.use(express.json({limit: '0.151mb'}));
 app.use(express.urlencoded({limit: '0.151mb', extended: true}));
 app.use(cookieParser())
-app.use("/posts", postsRouter)
-app.use(authrouter)
+app.use("/api/posts", postsRouter)
+app.use("/api", authrouter)
 mongoose.connect(process.env.MONGODB_URI)
-
-app.get("/", (req, res) => {
-    res.sendFile(
-        path.join(__dirname, "./frontend/build/index.html"),
-        (err) => {
-            if (err){
-                res.status(500).send(err)
-            }
-        }
-    )
-})
 
 app.listen(8000, "0.0.0.0", () => {
     console.log("Server is running.")
